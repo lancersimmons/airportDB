bd5a8fc812d2384d058c52fdc06d479d73119732
unknown <simmlr11@SIMMLR11-2013.deacnet.wfu.edu>
2017-04-12 19:35:56 -0400

    misc

--- loadTables/random.py
+++ loadTables/random.py
@@ -8,6 +8,11 @@ import sys
 print( 'Number of arguments:', len(sys.argv), 'arguments.')
 print( 'Argument List:', str(sys.argv))
 
+print(len(sys.argv))
+if len(sys.argv < 2):
+	print("Bad number of args")
+	exit()
+
 print(sys.argv[1])
 
 
@@ -19,5 +24,25 @@ print(sys.argv[1])
 # StreetAddress,City,State,CountryFull,
 # Username,Password,Birthday,EmailAddress,TelephoneNumber
 
-# fileHandler = open("filename", "r")
-# for line in 
+fileHandler1 = open(sys.argv[1], "r")
+splitLines = []
+for line in fileHandler1:
+    for char in line:
+        if char in "\"":
+            line = line.replace(char,'')
+    splitLine = line.split(",")
+    splitLines.append(splitLine)
+
+# insert into TABLE values(01,'varchar1','varchar2' );
+
+for element in splitLines:
+    print( element)
+
+
+
+
+
+
+
+
+
